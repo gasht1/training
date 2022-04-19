8d5e7283deb2cc2d373d9445e6a42f0bfc710c2d
gesht1 <gebriegashaw7@gmail.com>
2022-04-19 22:14:45 +0300

    scond commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
-
 const App = () => {
-  //https://fakestoreapi.com/products
-  const fakestore = async () => {
-    const response = await fetch("https://fakestoreapi.com/products");
-    console.log(response);
-  };
+const [update, setupdate] = useState([])
+
+console.log(update);
+useEffect(() => {
+
   fakestore();
+ 
+}, [])
+
+  
+
+
+  //https://fakestoreapi.com/products
+  const fakestore = async() => {
+    const response = await fetch("https://fakestoreapi.com/products");
+    //console.log(response);
+    const data= await response.json();
+    //console.log(data);
+    setupdate(data);
+
+  };
+ //
   return (
     <div className="container">
-      <div className="box">
+      {update.map((income)=>{
+        return(
+          <>
+          <div className="box">
         <div className="content">
-          <h2>title</h2>
-          <p>describtion</p>
+        <h2>{income.title}</h2>
+          <p>{income.description}</p>
         </div>
-        <h5>image source</h5>
+        <img src={income.image} alt="hi" />
       </div>
+      </>
+        )
+      })}
+      
     </div>
   );
 };
